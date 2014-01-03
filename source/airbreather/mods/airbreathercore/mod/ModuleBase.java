e46d2703bb026e6206d017d8177760398f5eea66
Joe Amenta <airbreather@linux.com>
2014-01-03 08:00:18 -0500

    Removing bindings for configuration classes.

--- source/airbreather/mods/airbreathercore/mod/ModuleBase.java
+++ source/airbreather/mods/airbreathercore/mod/ModuleBase.java
@@ -3,14 +3,11 @@ package airbreather.mods.airbreathercore.mod;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
-import airbreather.mods.airbreathercore.event.EventConfiguration;
 import airbreather.mods.airbreathercore.event.EventSubscriber;
 import airbreather.mods.airbreathercore.event.ForgeEventSubscriber;
 import airbreather.mods.airbreathercore.item.FmlItemRegistry;
-import airbreather.mods.airbreathercore.item.ItemConfiguration;
 import airbreather.mods.airbreathercore.item.ItemRegistry;
 import airbreather.mods.airbreathercore.recipe.FmlRecipeRegistrar;
-import airbreather.mods.airbreathercore.recipe.RecipeConfiguration;
 import airbreather.mods.airbreathercore.recipe.RecipeRegistrar;
 
 public class ModuleBase extends AbstractModule
@@ -21,8 +18,5 @@ public class ModuleBase extends AbstractModule
         this.bind(EventSubscriber.class).to(ForgeEventSubscriber.class).in(Scopes.SINGLETON);
         this.bind(RecipeRegistrar.class).to(FmlRecipeRegistrar.class);
         this.bind(ItemRegistry.class).to(FmlItemRegistry.class);
-        this.bind(ItemConfiguration.class).toProvider(ItemConfigurationProvider.class);
-        this.bind(EventConfiguration.class).toProvider(EventConfigurationProvider.class);
-        this.bind(RecipeConfiguration.class).toProvider(RecipeConfigurationProvider.class);
     }
 }
